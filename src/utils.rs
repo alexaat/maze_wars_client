3cdcd367b98c0ce2fe93df55d935ea48ac47bf8a
alexaat <alexaat@yahoo.co.uk>
2025-08-27 19:54:08 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,35 +1,35 @@
-use std::fs;
 use macroquad::prelude::{vec3, Vec3};
-use std::{f64::consts::PI};
+use std::f64::consts::PI;
+use std::fs;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 //read content of the file into string
 pub fn read_file(file_path: &str) -> Result<std::string::String, std::io::Error> {
     fs::read_to_string(file_path)
 }
 
-
 /*
     check that map
   - has all lines with same size,
   - comtains minimum 3 lanes,
   - contains at least one empty cell
-*/ 
-pub fn is_map_valid(content: &String) -> bool{
+*/
+pub fn is_map_valid(content: &String) -> bool {
     let mut len: usize = 0;
     let mut num_of_lines: u32 = 0;
     let mut num_of_empty: u32 = 0;
-    for line in content.lines(){
-        for ch in line.chars(){
+    for line in content.lines() {
+        for ch in line.chars() {
             if ch == ' ' {
-                num_of_empty+=1;
+                num_of_empty += 1;
             }
         }
 
         num_of_lines += 1;
-        if len == 0{
+        if len == 0 {
             len = line.len();
         } else {
-            if len != line.len(){
+            if len != line.len() {
                 return false;
             }
         }
@@ -42,21 +42,20 @@ pub fn is_map_valid(content: &String) -> bool{
     }
     if num_of_empty == 0 {
         return false;
-    }   
+    }
 
-    true    
-    
+    true
 }
 
 //converts srting conttents into vector of vectots whre "true" represents a wall and "false" represents empty cell
-pub fn map_to_slice(content: &String) -> Vec<Vec<bool>>{
+pub fn map_to_slice(content: &String) -> Vec<Vec<bool>> {
     let mut map = vec![];
-    for line in content.lines(){
+    for line in content.lines() {
         let mut l = vec![];
-        for ch in line.chars(){
-            if ch == ' '{
+        for ch in line.chars() {
+            if ch == ' ' {
                 l.push(false);
-            }else {
+            } else {
                 l.push(true);
             }
         }
@@ -65,17 +64,13 @@ pub fn map_to_slice(content: &String) -> Vec<Vec<bool>>{
     map
 }
 
-pub fn generate_up_to(num: usize) -> usize{
+pub fn generate_up_to(num: usize) -> usize {
     rand::random_range(0..num)
 }
 
-pub fn orientaion_to_degrees(orientation: Vec3) -> f32{
+pub fn orientaion_to_degrees(orientation: Vec3) -> f32 {
     //orientation
-    let o = vec3(
-        orientation.x,
-        orientation.y,
-        orientation.z,
-    );
+    let o = vec3(orientation.x, orientation.y, orientation.z);
     //same direction of arrow as on png
     let n = vec3(0.0, 0.0, -1.0);
     let cos_theta = o.dot(n);
@@ -87,7 +82,6 @@ pub fn orientaion_to_degrees(orientation: Vec3) -> f32{
     }
     theta
 }
-
 
 pub fn is_valid_ip_char(c: char) -> bool {
     if c >= '0' && c <= '9' {
@@ -101,4 +95,36 @@ pub fn is_valid_ip_char(c: char) -> bool {
 
 pub fn is_valid_name_char(c: char) -> bool {
     c >= ' ' && c <= '~'
+}
+
+pub fn get_ms() -> Option<u128> {
+    if let Ok(t) = SystemTime::now().duration_since(UNIX_EPOCH) {
+        return Some(t.as_millis());
+    }
+    None
+}
+
+pub fn get_settings(move_speed: &mut f32, look_speed: &mut f32) {
+    let content = read_file("settings");
+    if let Ok(_content) = content {
+        let lines = _content.lines();
+        for line in lines {
+            let parts: Vec<&str> = line.split(":").collect();
+            if parts.len() == 2 {
+                let key = parts[0].trim();
+                let value = parts[1].trim();
+
+                if String::from(key) == String::from("MOVE_SPEED") {
+                    if let Ok(_value) = value.parse::<f32>() {
+                        *move_speed = _value;
+                    }
+                }
+                if String::from(key) == String::from("LOOK_SPEED") {
+                    if let Ok(_value) = value.parse::<f32>() {
+                        *look_speed = _value;
+                    }
+                }
+            }
+        }
+    }
 }
